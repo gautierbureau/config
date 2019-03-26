31ca057db9ca2253ef7bd04111ca64aecfe1055b
Gautier Bureau <gautier.bureau@gmail.com>
2019-03-26 10:04:37 +0100

    corrected mistake in fi.

--- deploy_config.sh
+++ deploy_config.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+if [ -d "~/Config" ]; then
+  rm -rf ~/Config
+fi
 git clone https://github.com/gautierbureau/config.git ~/Config
-if [ -f "~/.bashrc" ];
+if [ -f "~/.bashrc" ]; then
   mv ~/.bashrc ~/.bashrc.old
 fi
 ln -s Config/bashrc ~/.bashrc
-if [ -f "~/.zshrc" ];
+if [ -f "~/.zshrc" ]; then
   mv ~/.zshrc ~/.zshrc.old
 fi
 ln -s Config/zshrc-oh-my-zsh.zsh ~/.zshrc
-if [ -f "~/.gitconfig" ];
+if [ -f "~/.gitconfig" ]; then
   mv ~/.gitconfig ~/.gitconfig.old
 fi
 ln -s Config/gitconfig ~/.gitconfig
