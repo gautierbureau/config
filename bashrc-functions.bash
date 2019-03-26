2cdf2cd1aaec1b86b7c734e9b2c842a828104ce1
Gautier Bureau <gautier.bureau@gmail.com>
2019-03-26 20:05:18 +0100

    Added functions for patch in git

--- bashrc-functions.bash
+++ bashrc-functions.bash
@@ -6,6 +6,50 @@ see_bash_options() {
 	shopt | grep on
 }
 
+create_patch_unstaged() {
+	if [ -z "$1" ]; then
+		echo "You need a filename."
+		exit 1
+	fi
+	git diff > $1
+}
+
+create_patch_staged() {
+	if [ -z "$1" ]; then
+		echo "You need a filename."
+		exit 1
+	fi
+	git diff --cached > $1
+}
+
+create_patch_all() {
+	if [ -z "$1" ]; then
+		echo "You need a filename."
+		exit 1
+	fi
+	git diff HEAD > $1
+}
+
+create_patch_last_commit() {
+	git format-patch -1
+}
+
+create_patch_n_commit() {
+	if [ -z "$1" ]; then
+		echo "You need a filename."
+		exit 1
+	fi
+	git format-patch -$1
+}
+
+apply_patch() {
+	if [ -z "$1" ]; then
+		echo "You need a filename."
+		exit 1
+	fi
+	git apply $1
+}
+
 if [ -f ~/Config/bashrc-functions-rte.bash ]; then
   . ~/Config/bashrc-functions-rte.bash
 fi
