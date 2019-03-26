2e39e59b03686fa1491f690d32391c7c613e9b5d
Gautier Bureau <gautier.bureau@gmail.com>
2019-03-26 10:03:17 +0100

    Corrected script and added readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+Command to execute
+``` bash
+$> bash <(curl -s https://raw.githubusercontent.com/gautierbureau/config/github/deploy_config.sh)
+```
--- deploy_config.sh
+++ deploy_config.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 git clone https://github.com/gautierbureau/config.git ~/Config
+if [ -f "~/.bashrc" ];
+  mv ~/.bashrc ~/.bashrc.old
+fi
 ln -s Config/bashrc ~/.bashrc
+if [ -f "~/.zshrc" ];
+  mv ~/.zshrc ~/.zshrc.old
+fi
 ln -s Config/zshrc-oh-my-zsh.zsh ~/.zshrc
-ln -s Config/gitconfig .gitconfig
+if [ -f "~/.gitconfig" ];
+  mv ~/.gitconfig ~/.gitconfig.old
+fi
+ln -s Config/gitconfig ~/.gitconfig
